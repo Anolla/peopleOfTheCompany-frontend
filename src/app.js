6344b79c8affb34bc463d352c986057d3935c57c
Anolla <annehaddad96@yahoo.com>
2021-09-17 22:14:24 +0300

    added font

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from "react";
 import TableComponent from "./components/table";
 import NavBarComponent from "./components/navBar";
 import Footer from "./components/footer";
+import "./app.css";
 
 function App() {
   return (
